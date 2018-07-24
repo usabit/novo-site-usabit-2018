89b0e7af0a67ab60a883769d126a1c251a2f288e
Douglas Oliveira <douglas@usabit.com.br>
2018-07-24 12:40:07 -0300

    Menu

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -1,4 +1,16 @@
 (function ($, window, document) {
     'use strict';
     console.log('common.js init;');
+    // side navigation
+    $('.site-header__open-menu').on('click', function(e) {
+        e.preventDefault();
+        $('.sidenav').addClass('active');
+        $('html').css('overflow', 'hidden');
+    });
+
+    $('.sidenav__close-menu').on('click', function(e) {
+        e.preventDefault();
+        $('.sidenav').removeClass('active');
+        $('html').css('overflow', 'initial');
+    });
 })(jQuery, window, document);
