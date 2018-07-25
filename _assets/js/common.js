6e24904a4f5ea1058a43275b8661e2756c8b27c7
Douglas Oliveira <douglas@usabit.com.br>
2018-07-24 21:42:27 -0300

    Ajustes

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -2,20 +2,6 @@
     'use strict';
     console.log('common.js init;');
 
-    // Navbar Scroll Animation
-    $(window).on('scroll', function(){
-        if($(window).scrollTop() > 0){
-            $('header.site-header').css({
-                transition: 'all 500ms',
-                background: '#1c1c1c'
-            });
-        }else{
-            $('header.site-header').css({
-                background: 'transparent'
-            });
-        }
-    });
-
     // side navigation
     $('.site-header__open-menu').on('click', function(e) {
         e.preventDefault();
