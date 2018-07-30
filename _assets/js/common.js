64549ce6708d19aa1b2d80f1569bc48e08d1deb8
Douglas Oliveira <douglas@usabit.com.br>
2018-07-30 17:50:41 -0300

    Review in services section

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -15,38 +15,12 @@
         $('html').css('overflow', 'initial');
     });
 
-    /* Effect fadeOut
-    $(window).scroll(function() {
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if(scrollTop > 420){
-            $('.banner-intro').css({ 'position': 'sticky'} );
-        }else{
-            $('.banner-intro').css({ 'position': 'initial'} );
-        }
-        $('.effect-fadeout').each(function(){
-            if($(this).offset().top - scrollTop < 70){
-                if(!$(this).hasClass('effect-fadeout-active')){
-                    $(this).addClass('effect-fadeout-active');
-                }
-            } else {
-                if($(this).hasClass('effect-fadeout-active')){
-                    $(this).removeClass('effect-fadeout-active');
-                }
-            }
-        });
-    });
-    */
-
     // Navbar scroll animação
     $(window).on('scroll', function () {
         if ($(window).scrollTop() > 0) {
             $('header.site-header').css({
                 transition: 'all 500ms',
                 background: '#1c1c1c',
-                //backgroundImage: 'url("assets/img/background/banner-header.jpg")',
-                //backgroundRepeat: 'no-repeat',
-                //backgroundPosition: 'center center',
-                //backgroundSize: 'cover',
                 color: '#ffffff'
             });
         } else {
